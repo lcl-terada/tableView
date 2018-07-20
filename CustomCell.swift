68e0b37d421e9c0f8e7e3d8988cdeb97200d8554
teradakunio <terada@lclco.com>
2018-07-20 11:07:23 +0900

    badgeWrapperViewを追加

--- CustomCell.swift
+++ CustomCell.swift
@@ -11,33 +11,80 @@ import UIKit
 class CustomCell: UITableViewCell, UITextFieldDelegate {
 
     let titleLabel = UILabel()
+    let dateLabel = UILabel()
+    let thumbnailImageView = UIImageView(image: nil)
+    let badgeView = UIView()
+    let badgeWrapperView = UIView()
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        badgeView.backgroundColor = UIColor.brown
+        //　背景色の設定
+        badgeView.translatesAutoresizingMaskIntoConstraints = false
+        // trueだとコンフリクトしてしまう
+        self.addSubview(badgeView)
+        // addSubviewは
+        
+        badgeWrapperView.backgroundColor = UIColor.cyan
+        badgeWrapperView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(badgeWrapperView)
+        
+        thumbnailImageView.backgroundColor = UIColor.yellow
+        thumbnailImageView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(thumbnailImageView)
+        
         titleLabel.backgroundColor = UIColor.green
-            //trueだとコンフリクトしてしまう
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         self.addSubview(titleLabel)
-            //NSLayoutAnchorを用いたコードによるAuto Layout
+        
+        dateLabel.backgroundColor = UIColor.red
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(dateLabel)
+        
+        
+        
+        badgeView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10.0).isActive = true
+        badgeView.trailingAnchor.constraint(equalTo: thumbnailImageView.leadingAnchor, constant: 0.0).isActive = true
+        badgeView.widthAnchor.constraint(equalToConstant: 25.0).isActive = true
+        badgeView.heightAnchor.constraint(equalToConstant: 50.0).isActive = true
+        badgeView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+//        thumbnailImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10.0).isActive = true
+        thumbnailImageView.trailingAnchor.constraint(equalTo: titleLabel.leadingAnchor, constant: 0.0).isActive = true
+        thumbnailImageView.trailingAnchor.constraint(equalTo: dateLabel.leadingAnchor, constant: 0.0).isActive = true
+        //equalToだとcell + 50.0になるが、equalToConstantだと50.0のみで表示される。
+        thumbnailImageView.widthAnchor.constraint(equalToConstant: 50.0).isActive = true
+        thumbnailImageView.heightAnchor.constraint(equalToConstant: 50.0).isActive = true
+        thumbnailImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         
         titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 10.0).isActive = true
-        titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10.0).isActive = true
+        //titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10.0).isActive = true
         titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10.0).isActive = true
-        titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10.0).isActive = true
+        // この文を消すことによって一つのセルに２つの文が入る
+//        titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10.0).isActive = true
         titleLabel.numberOfLines = 0
-        
         titleLabel.sizeToFit()
-    }
 
+        // ２つの文を一つのセルにいれるときは、上の文のbottomAnchorを消して、下の文のtopAnchorをtitleLabel.bottomAnchorとしていれる。
+        dateLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 0.0).isActive = true
+        //dateLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10.0).isActive = true
+        dateLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10.0).isActive = true
+        dateLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0.0).isActive = true
+        dateLabel.numberOfLines = 0
+        dateLabel.sizeToFit()
+    }
+    //19,20行目とセット
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    func configure(text: String){
-        titleLabel.text = text
-        print(text)
+    // 新しい関数を定義（関数の中に関数はできない）
+    func configure(title: String, date: String){
+        
+        titleLabel.text = title
+        dateLabel.text = date        
     }
+    
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
--- tableview/ViewController.swift
+++ tableview/ViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
     
-    var items = ["アップデートのお知らせ ver2.7閲覧履歴を追加しました","アップロードしていただき、ありがとうございます"]
-    var section: [String] = [""]
+    var items = [
+        (title: "応募受付8/6まで！書籍【全国 高速バスの不思議と謎】プレゼントのご案内", date: "2018/07/19"),
+        (title: "アップデートのお知らせ ver2.7閲覧履歴を追加しました", date: "2018/06/19"),
+        (title:"【プレゼントキャンペーン実施中】アンケートに答えて高速バス乗車便利グッズをもらおう！", date: "2017/09/06"),
+        (title:"【ニュース】［画像有り+シェア有り］JRバス関東　高速バス指定席乗車券にQRコード添付でスムーズな乗車が可能に", date: "2016/06/20"),
+        (title: "アップロードしていただき、ありがとうございます", date: "高速バス比較開発チーム")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,10 +24,14 @@ class ViewController: UIViewController, UITableViewDataSource {
         let tableView: UITableView = UITableView()
         tableView.dataSource = self
         tableView.register(CustomCell.self, forCellReuseIdentifier: "CustomCell")
+        // オートレイアウト時にUIViewのレイアウトが変更しないようにする
         tableView.translatesAutoresizingMaskIntoConstraints = false
+        // カスタマイズビューを追加
         view.addSubview(tableView)
+        // コードが実行できているかbackgroundColorで判断する
         view.backgroundColor = UIColor.red
         
+        // オートレイアウトでtableViewを画面いっぱいに表示
         tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
@@ -30,24 +39,27 @@ class ViewController: UIViewController, UITableViewDataSource {
        
         tableView.estimatedRowHeight = 20
         tableView.rowHeight = UITableViewAutomaticDimension
-        
         tableView.reloadData()
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-        //一つのsectionの中に入れるCellの数を決める
+    //　一つのsectionの中に入れるCellの数を決める
     func tableView(_ tableView:UITableView,numberOfRowsInSection section: Int) -> Int{
-        //itemsにある要素数
+        //　itemsにある要素数
         return items.count
     }
-        //Cellの内容をきめる
+    //　Cellの内容をきめる
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        //CustomCellを呼ぶ
+        //　CustomCellを呼ぶ
         let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! CustomCell
-        cell.configure(text: items[indexPath.row])
+        // items内にあるtitleとdateを順に出す
+        cell.configure(title: items[indexPath.row].title, date: items[indexPath.row].date)
+        
         cell.layoutIfNeeded()
         return cell
     }
